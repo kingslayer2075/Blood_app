2b8f4980e9351c04ebe172d16460e81c0c5aa85f
akida1023 <kdakida1023@gmail.com>
2025-05-01 23:36:05 -0500

    imagenes e interaccion botones

--- app/src/main/java/com/example/blood_app/ui/theme/PrincipalPageActivity.kt
+++ app/src/main/java/com/example/blood_app/ui/theme/PrincipalPageActivity.kt
@@ -2,10 +2,21 @@ package com.example.blood_app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.widget.ImageButton
+import com.example.blood_app.ui.theme.PpmActivity
+
 
 class PrincipalPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.principal_page)
+
+        val ppmbButton = findViewById<ImageButton>(R.id.ppmbButton)
+        ppmbButton.setOnClickListener {
+            val intent = Intent(this, PpmActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
